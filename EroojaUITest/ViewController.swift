1a875ec8e6fe9e74636e5c9a418c9cab1bfd2eca
denny.k <denny.k@kakaocorp.com>
2020-03-30 23:48:05 +0900

    [ERI-012] EroojaDev Build Target 설정

---
+++ EroojaUITest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EroojaUITest
+//
+//  Created by 김태인 on 2020/03/30.
+//  Copyright © 2020 김태인. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
