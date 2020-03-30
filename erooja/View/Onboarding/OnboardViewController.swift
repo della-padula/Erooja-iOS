88bb181a32e99a9273a202c59722e6d3c73ee114
denny.k <denny.k@kakaocorp.com>
2020-03-31 00:15:24 +0900

    [ERI-013] Erooja DEV Main VC 구성, Enum활용한 목록 구성

---
+++ erooja/View/Onboarding/OnboardViewController.swift
@@ -0,0 +1,15 @@
+//
+//  OnboardViewController.swift
+//  erooja
+//
+//  Created by 김태인 on 2020/03/30.
+//  Copyright © 2020 김태인. All rights reserved.
+//
+
+import Foundation
+
+class OnboardViewController: UIViewController {
+    override func viewDidLoad() {
+        
+    }
+}
