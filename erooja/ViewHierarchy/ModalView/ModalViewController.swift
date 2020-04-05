bdb6c0ea62ab574ac7a72ba8b894c952ee3e8d48
denny.k <denny.k@kakaocorp.com>
2020-04-05 14:34:47 +0900

    [ERI-022] Common Modal View Controller Implemented

--- erooja/ViewHierarchy/ModalView/ModalViewController.swift
+++ erooja/ViewHierarchy/ModalView/ModalViewController.swift
@@ -23,11 +23,18 @@ public class ModalViewController: BaseViewController {
         imageButton.image = .commonIcoClose
         imageButton.padding = 10
         imageButton.backgroundColor = .green
+        imageButton.addTarget(target: self, action: #selector(onClickCloseButton), forEvent: .touchUpInside)
+        
         self.view.addSubview(imageButton)
         imageButton.translatesAutoresizingMaskIntoConstraints = false
         imageButton.heightAnchor.constraint(equalToConstant: 44).isActive = true
         imageButton.widthAnchor.constraint(equalTo: imageButton.heightAnchor, multiplier: 1.0).isActive = true
         imageButton.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor).isActive = true
         imageButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+    }
+    
+    @objc
+    public func onClickCloseButton() {
+        self.dismiss(animated: true, completion: nil)
     }
 }
