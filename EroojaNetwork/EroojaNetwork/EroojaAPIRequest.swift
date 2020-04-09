b4f4e387e68f0e7c702a5f3e75e9138ac5ae7ca8
denny.k <denny.k@kakaocorp.com>
2020-04-10 00:26:38 +0900

    Service Login API Request Implemented

--- EroojaNetwork/EroojaNetwork/EroojaAPIRequest.swift
+++ EroojaNetwork/EroojaNetwork/EroojaAPIRequest.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import Alamofire
+import EroojaCommon
 
 public enum KakaoTokenReqType {
     case id
@@ -99,5 +100,16 @@ public enum EroojaAPIError: Error {
 }
 
 public class EroojaAPIRequest {
-    
+    func requestWithJSON(jsonData: [String: String], completion: @escaping (Result<[String : String], EroojaAPIError>) -> Void) {
+        AF.request(AuthAPIRequest.RequestType.kakaoToken(.id, jsonData["kakaoId"]!).requestURL, method: .post, parameters: jsonData, encoder: JSONParameterEncoder.default).response(completionHandler: { response in
+            switch response.result {
+            case .success(_):
+                completion(.success(["token":"sample"]))
+                break
+            case .failure(let error):
+                ELog.error(message: error.localizedDescription)
+                completion(.failure(.urlRequestError)) // TEMP Error
+            }
+        })
+    }
 }
