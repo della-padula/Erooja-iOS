5b45aa6587d90671f5908246a8e74d63e2da8793
denny.k <denny.k@kakaocorp.com>
2020-04-10 00:04:47 +0900

    Kakao Token, Refresh API Request appending Path Components

--- EroojaNetwork/EroojaNetwork/AuthAPIRequest.swift
+++ EroojaNetwork/EroojaNetwork/AuthAPIRequest.swift
@@ -10,40 +10,44 @@ import Foundation
 
 public struct AuthAPIRequest {
     enum RequestType {
-        case kakaoToken(KakaoTokenReqType)
+        case kakaoToken(KakaoTokenReqType, String)
         case login
         case refreshToken
         
         
         var requestURL: URL{
             let compositeRequestURL: URL
-            let baseContactsURL = URL(string: EURLConstant.hostURLString)!
+            let baseURL = URL(string: EURLConstant.hostURLString)!
             switch self {
-            case let .kakaoToken(type):
-                compositeRequestURL = baseContactsURL.appendingPathComponent("auth").appendingPathComponent("kakao")
+            case let .kakaoToken(type, _):
+                let tempRequestURL = baseURL.appendingPathComponent("auth").appendingPathComponent("kakao")
                 if type == .id {
-                    compositeRequestURL.appendingPathComponent("")
+                    compositeRequestURL = tempRequestURL.appendingPathComponent("?by=ID")
                 } else {
-                    
+                    compositeRequestURL = tempRequestURL.appendingPathComponent("?by=ACCESS_TOKEN")
                 }
-                break
-            default:
-                break
+            case .login:
+                // MARK: TEMP (Need to be updated)
+                compositeRequestURL = baseURL
+            case .refreshToken:
+                compositeRequestURL = baseURL.appendingPathComponent("token").appendingPathComponent("refresh")
             }
+            return compositeRequestURL
         }
         
         var requestParameter: [String: Any] {
             var parameters: [String: Any] = [:]
             switch self {
-            case let .kakaoToken(type):
+            case let .kakaoToken(type, value):
                 if type == .id {
-                    parameters["by"] = "ID"
+                    parameters["kakaoId"] = value
                 } else {
-                    parameters["by"] = "ACCESS_TOKEN"
+                    parameters["accessToken"] = value
                 }
             default:
                 break
             }
+            return parameters
         }
     }
 }
