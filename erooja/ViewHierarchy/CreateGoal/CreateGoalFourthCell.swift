5178010abb206f5f61890532a0d89a880f76907c
denny.k <denny.k@kakaocorp.com>
2020-04-12 21:40:02 +0900

    [ERI-015] Date Picker Integration with endDate Label

--- erooja/ViewHierarchy/CreateGoal/CreateGoalFourthCell.swift
+++ erooja/ViewHierarchy/CreateGoal/CreateGoalFourthCell.swift
@@ -6,11 +6,22 @@
 //  Copyright © 2020 김태인. All rights reserved.
 //
 
+import NotificationCenter
 import EroojaCommon
 import EroojaUI
 
 public class CreateGoalFourthCell: UICollectionViewCell {
+    @IBOutlet weak var lblStartDate: UILabel!
+    @IBOutlet weak var lblEndDate: UILabel!
+    
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var calendarButton: UIButton!
+    
+    @IBAction func onClickCalendar(_ sender: Any) {
+        NotificationCenter.default.post(
+        name: NSNotification.Name(rawValue: "CalendarButton"),
+        object: nil)
+    }
     
     public var titleText: String? {
         didSet {
@@ -20,7 +31,20 @@ public class CreateGoalFourthCell: UICollectionViewCell {
     
     override public func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+
+        NotificationCenter.default.addObserver(self,
+        selector: #selector(didReceiveTestNotification(_:)),
+        name: NSNotification.Name("CGDatePickerSelected"),
+        object: nil)
+    }
+     
+    @objc func didReceiveTestNotification(_ notification: Notification) {
+        guard let selectedDate: String = notification.userInfo?["SelectedDate"] as? String else { return }
+        
+        print("SelectedDate :", selectedDate)
+        lblEndDate.text = selectedDate
     }
 
+    
+    
 }
