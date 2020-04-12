866c68f07844a60bd7b71ce2fdbb9f3568d1cf08
denny.k <denny.k@kakaocorp.com>
2020-04-12 10:05:31 +0900

    Create Goal View Controller is Created

---
+++ erooja/ViewHierarchy/CreateGoal/CreateGoalViewController.swift
@@ -0,0 +1,21 @@
+//
+//  CreateGoalViewController.swift
+//  erooja
+//
+//  Created by 김태인 on 2020/04/12.
+//  Copyright © 2020 김태인. All rights reserved.
+//
+
+import Foundation
+import EroojaUI
+import EroojaCommon
+
+public class CreateGoalViewController: BaseViewController {
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override public func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+}
---
+++ erooja/ViewHierarchy/CreateGoal/CreateGoalViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  CreateGoalViewModel.swift
+//  erooja
+//
+//  Created by 김태인 on 2020/04/12.
+//  Copyright © 2020 김태인. All rights reserved.
+//
+
+import Foundation
+import EroojaNetwork
+import EroojaSharedBase
+import EroojaCommon
+
+public class CreateGoalViewModel {
+    
+}
+
+extension CreateGoalViewModel {
+    
+}
