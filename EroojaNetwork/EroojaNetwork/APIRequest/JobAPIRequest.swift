f8b5d7b11c1a05804494211f17d6e1c5f9c1cf58
denny.k <della.kimko@gmail.com>
2020-05-01 23:47:58 +0900

    Implementation of API URL generation module related to Job Group and Job Single Item Fetch

---
+++ EroojaNetwork/EroojaNetwork/APIRequest/JobAPIRequest.swift
@@ -0,0 +1,35 @@
+//
+//  GoalAPIRequest.swift
+//  EroojaNetwork
+//
+//  Created by Denny on 2020/05/01.
+//  Copyright © 2020 김태인. All rights reserved.
+//
+
+import Foundation
+import EroojaCommon
+
+public struct JobAPIRequest {
+    enum RequestType {
+        // 직군 불러오기
+        case fetchJobGroupList
+        // 직군, 직무 함께 조회
+        case fetchJobListFromGroupId(Int)
+        // 직군,직무 단일 조회
+        case fetchJobFromItemId(Int)
+        
+        var requestURL: URL{
+            let compositeRequestURL: URL
+            let baseURL = URL(string: EroojaURLConstant.hostURLString)!
+            switch self {
+            case .fetchJobGroupList:
+                compositeRequestURL = baseURL.appendingPathComponent("jobGroup")
+            case .fetchJobListFromGroupId(let value):
+                compositeRequestURL = baseURL.appendingPathComponent("jobGroup/\(value)")
+            case .fetchJobFromItemId(let value):
+                compositeRequestURL = baseURL.appendingPathComponent("jobInterest/\(value)")
+            }
+            return compositeRequestURL
+        }
+    }
+}
